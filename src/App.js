13e607ca93ffb5ff97e5ec5b3e1d85539d94f17b
Sergey Sokolov <sokolovs@gmail.com>
2020-07-01 20:01:50 -0700

    feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There Monkeys.
+          Hi There Monkeys.Feature branch.
         </p>
         <a
           className="App-link"
